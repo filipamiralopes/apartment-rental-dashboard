5ee7200d1f39e0e13a90cd7fb4c584a211c0a55f
DaniDias95 <guiguimu_art@hotmail.com>
2024-05-28 16:41:37 +0100

    navbar started

---
+++ apartment-rental-dashboard/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import logoOne from "../assets/logo.png"
+
+const Navbar = () => {
+    //#361236
+  return (
+    <div className='navbar'>
+    <img src={logoOne} alt="apartment rentals logo" style={{height:60}}/>
+
+    <h2>Apartment Rental Lopes&Dias</h2>
+    
+    <span> </span>
+    
+    </div>
+  )
+}
+
+export default Navbar
