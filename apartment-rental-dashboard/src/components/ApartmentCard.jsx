30a93d300696435e4b1d3ed43e776cfb0d918815
DaniDias95 <guiguimu_art@hotmail.com>
2024-05-29 19:07:28 +0100

    css done

--- apartment-rental-dashboard/src/components/ApartmentCard.jsx
+++ apartment-rental-dashboard/src/components/ApartmentCard.jsx
@@ -1,32 +1,44 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBed, faBath, faUsers, faStar } from '@fortawesome/free-solid-svg-icons';
 
-const ApartmentCard = ({rental, onDelete}) => {
+const ApartmentCard = ({ rental, onDelete }) => {
   return (
-    <div key={rental.id} className="rental-unit">
-    <img src={rental.picture_url.url} />
-
-    <div className="unit-description">
-      <h4>
-        {rental.name} 
-      </h4>
-      {/* <p><i>{rental.space}</i></p> */}
-      <p>
-        <b>Price: </b>
-        {rental.price}€
-      </p>
-      <div className="labels">
-        {rental.price <= 85 ? (
-          <p className="green-label">Great Deal</p>
-        ) : rental.price >= 150 ? (
-          <p className="blue-label">Premium</p>
-        ) : (
-          <p></p>
-        )}
-        {rental.review_scores_rating > 90 ? <p className="blue-label">Super Host!</p> : <p></p>}
+    <div className="rental-unit">
+      <div className="img-box-rental">
+        <img src={rental.picture_url.url} alt={`Photo of ${rental.name}`} />
       </div>
-      <button onClick={()=>{onDelete(rental.id)}}>Delete</button>
+      <div className="unit-description">
+        <div className="title">
+          <h3>{rental.name}</h3>
+          <h2 className="rating"><FontAwesomeIcon icon={faStar} /> {rental.review_scores_rating}/100</h2>
+        </div>
+        <p><b>Location:</b> {rental.city}, {rental.country}</p>
+        <div className="unit-icons">
+          <p><FontAwesomeIcon icon={faBed} /> {rental.bedrooms}</p>
+          <p><FontAwesomeIcon icon={faBath} /> {rental.bathrooms}</p>
+          <p><FontAwesomeIcon icon={faUsers} /> {rental.accommodates}</p>
+        </div>
+        <div className="price-rating">
+          <p className="price">{rental.price}€</p>
+        </div>
+        <div className="labels">
+          {rental.price <= 85 ? (
+            <p className="great-deal-label">Great Deal!</p>
+          ) : rental.price >= 150 ? (
+            <p className="premium-label"><strong>Premium</strong></p>
+          ) : (
+            <span></span>
+          )}
+          {rental.review_scores_rating > 90 ? (
+            <p className="super-host-label"><strong>Super Host!</strong></p>
+          ) : (
+            <span></span>
+          )}
+        </div>
+        <button onClick={() => onDelete(rental.id)} className="delete-btn">Delete</button>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default ApartmentCard
+export default ApartmentCard;
