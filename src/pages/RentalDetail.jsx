f04e0dde75e76cecb3ebad708eb358aa677cbc30
Filipa <filipamnmlopes@hotmail.com>
2024-05-30 18:23:12 +0100

    add rent detail + CSS

--- src/pages/RentalDetail.jsx
+++ src/pages/RentalDetail.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from "react";
 import { useParams } from "react-router-dom";
 
 const RentalDetail = (props) => {
-  const {rentals} = props
+  const { rentals } = props;
   const { rentalId } = useParams();
   const filteredRentals = rentals.find((unit) => {
     if (unit.id == rentalId) {
@@ -11,8 +11,18 @@ const RentalDetail = (props) => {
   });
 
   return (
-    <div>{filteredRentals.name}</div>
-  )
-}
+    <div className="rental-detail">
+      <div id="rental-detail-image">
+        <img src={filteredRentals.picture_url.url} />
+      </div>
+      <h2>{filteredRentals.name}</h2>
+      <h4>{filteredRentals.neighbourhood}, {filteredRentals.city}</h4>
+      <h3>{filteredRentals.price},00€/por noche | Score: {filteredRentals.review_scores_rating}%</h3>
+      <p>{filteredRentals.host_name} | hoste since {filteredRentals.host_since} | Typically responds {filteredRentals.host_response_time}</p>
+      <p><b>Comentario del anunciante</b></p>
+      <p>{filteredRentals.space}</p>
+    </div>
+  );
+};
 
-export default RentalDetail
+export default RentalDetail;
