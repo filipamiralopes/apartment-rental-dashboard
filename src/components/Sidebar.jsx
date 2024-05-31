0163917bab64c1efd07115cbf47b3b346c37d4c6
Filipa <filipamnmlopes@hotmail.com>
2024-05-31 15:41:19 +0100

    add links to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 const Sidebar = () => {
   return (
     <div className="sidebar">
-        <Link to="/">Home Page</Link>
-        <a href="#About">About</a>
+      <Link to="/">Home Page</Link>
+      <Link to="/about">About</Link>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar  
+export default Sidebar;
