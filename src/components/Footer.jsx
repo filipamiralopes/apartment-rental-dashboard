6abd53d3a1ae06c657dfd43c667b0654fbbdfd1d
DaniDias95 <guiguimu_art@hotmail.com>
2024-06-01 11:35:47 +0100

    return home from notfound done, github link footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,10 @@ import githubLogo from '../assets/github-logo.png'
 const Footer = () => {
   return (
     <div className='footer'>
-        <img src={githubLogo} alt='white Github logo'/>
-        <p>Github Repository</p>
+      <a href="https://github.com/filipamiralopes/apartment-rental-dashboard" target="_blank">
+        <img src={githubLogo} alt='white Github logo'/></a>
+        <a href="https://github.com/filipamiralopes/apartment-rental-dashboard" target="_blank" className='footer-link'>
+        <p>Github Repository</p></a>
     </div>
   )
 }
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import notFound from '../assets/sad-robot.jpg'
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
     <div className='page-not-found'>
     <h1>404 - Page Not Found</h1>
       <p>Sorry, the page you are looking for could not be found.</p>
-
+      
       <img src={notFound} alt="page not found" className='robot-not-found'/>
-
+      <Link to="/">
       <button >Return Home</button>
-
+      </Link>
       </div>
   )
 }
