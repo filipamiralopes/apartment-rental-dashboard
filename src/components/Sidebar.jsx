9d3a891044e7864c38de2923cfd111151d1090ed
DaniDias95 <guiguimu_art@hotmail.com>
2024-06-05 15:37:26 +0100

    icons added to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,37 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBed,
+  faBath,
+  faUsers,
+  faStar,
+ faMagnifyingGlass,
+ faHouse,
+ faHeart,
+ faPlus,
+ faFile,
+
+} from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <Link to="/">Home Page</Link>
-      <Link to="/favourites">Favourites</Link>
-      <Link to="/add-rental">Add Rental</Link>
-      <Link to="/about">About</Link>
+      <div className="sidebar-links">
+      <Link to="/">
+      <FontAwesomeIcon icon={faHouse} /> <span> </span>Home Page</Link>
+      </div>
+      <div className="sidebar-links">
+      <Link to="/favourites">
+      <FontAwesomeIcon icon={faHeart} /> <span> </span> Favourites</Link>
+      </div>
+      <div className="sidebar-links">
+      <Link to="/add-rental">
+      <FontAwesomeIcon icon={faPlus} /> <span> </span> Add Rental</Link>
+      </div>
+      <div className="sidebar-links">
+      <Link to="/about">
+      <FontAwesomeIcon icon={faFile} /> <span> </span> About</Link>
+      </div>
     </div>
   );
 };
