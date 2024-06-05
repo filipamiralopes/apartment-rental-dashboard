bfeb1ee39da7bf8a1b59c930a0c9d338d563e32b
DaniDias95 <guiguimu_art@hotmail.com>
2024-06-05 15:51:37 +0100

    about done

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,7 +4,7 @@ const AboutPage = () => {
   return (
     <div className='about-project'>
       <h2>About the project</h2>
-      <p>Welcome to our React App mini project! Admin dashboard for the apartment rental platform where the admin can see and manage the apartments shown on the platform.</p>
+      <p id="text-ident">Welcome to our React App mini project! This admin dashboard is designed for an apartment rental platform, allowing administrators to view and manage the listed apartments efficiently. In this project, admins can create new apartment listings, update existing ones, delete listings, and add rentals to their favorites. We've chosen the color scheme based on the colors of Coimbra and Chamusca, significant places for us in Portugal.</p>
       
       <h2>Team Members</h2>
       <div className='team-members'>
@@ -18,7 +18,7 @@ const AboutPage = () => {
       <div className='member'>
         <img src="https://media.licdn.com/dms/image/D4D03AQGJzoPeHaTCJA/profile-displayphoto-shrink_800_800/0/1667037792619?e=1722470400&v=beta&t=O-tmWN9JQKGpbWHcO_crZX5laMputxKVOZNXXSbLUDQ" alt="Photo of Daniela" />
         <h3>Daniela Dias</h3>
-        <p>Background in veterinary assistance spanning five years, transitioned into managerial roles, handling tasks such as accounting, human resources, resource management, and inventory control. Seeking new horizons, embarked on a transformative journey, currently pursuing a comprehensive Full Stack Web Development program at Ironhack. In transitioning to web development, I aim to carve a path where creativity and impact converge.</p>
+        <p>Background in veterinary assistance spanning five years, transitioned into managerial roles, handling tasks such as accounting, human resources, resource management, and inventory control. In transitioning now to web development, I aim to carve a path where creativity and impact converge.</p>
         <a href="https://github.com/Dani-A-Dias">Daniela's GitHub</a><br/>
       <a href="https://www.linkedin.com/in/daniela-dias-604926243/">Daniela's LinkedIn</a><br/>
       
