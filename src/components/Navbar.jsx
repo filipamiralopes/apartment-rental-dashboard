6eb9676fc19dedf099eb41d6b7ff36d85804feef
DaniDias95 <guiguimu_art@hotmail.com>
2024-06-05 16:21:12 +0100

    updated apartment-list cards

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,22 @@
-import React from "react";
-import logoOne from "../assets/logo.png";
+import React from 'react';
+import logoOne from '../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  //#361236
-  return (
-    <div className="navbar">
-      <img src={logoOne} alt="apartment rentals logo" style={{ height: 60 }} />
-
-      <h2>Apartment Rental Lopes&Dias</h2>
-
-      <span> </span>
-    </div>
-  );
+	//#361236
+	return (
+		<div className="navbar">
+			<img
+				src={logoOne}
+				alt="apartment rentals logo"
+				style={{ height: 60 }}
+			/>
+			<Link to="/">
+				<h2>Apartment Rental Lopes&Dias</h2>
+			</Link>
+			<span> </span>
+		</div>
+	);
 };
 
 export default Navbar;
